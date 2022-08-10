8c39e40203435af8905b4db5ea011ec4820fb5e0
Afrizal <4friizal@gmail.com>
2022-08-10 14:14:19 +0700

    import font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
