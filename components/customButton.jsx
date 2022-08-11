8082f7ec95da9e6a5298eb694d2849b202343c1c
Afrizal <4friizal@gmail.com>
2022-08-11 19:55:02 +0700

    fixing authentication, component input & button

--- components/customButton.jsx
+++ components/customButton.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const AddButton = (props) => {
   return (
     <button
+      id={props.id}
       onClick={props.onClick}
-      className="btn btn-sm btn-wide btn-primary text-white font-Roboto mt-2 rounded-[20px] md:w-72 md:h-12 lg:w-[432px]"
+      className="btn btn-sm btn-primary w-full text-white font-Roboto mt-2 rounded-[20px] md:h-10 "
     >
       {props.title}
     </button>
@@ -15,10 +16,21 @@ const AddButton2 = (props) => {
   return (
     <button
       onClick={props.onClick}
-      className="btn btn-sm btn-outline btn-wide text-primary shadow-lg font-Roboto mt-2 rounded-[20px] md:h-12 md:w-72 lg:w-[432px]"
+      className="btn btn-sm btn-outline w-full text-primary shadow-lg font-Roboto mt-2 rounded-[20px] md:h-10"
     >
       {props.title}
     </button>
   );
 };
-export { AddButton2, AddButton };
+
+const DeleteButton = (props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className="btn btn-sm w-full btn-error text-white font-Roboto mt-2 rounded-[20px] md:h-10"
+    >
+      {props.title}
+    </button>
+  );
+};
+export { AddButton2, AddButton, DeleteButton };
