db5538b9b6ab351a08e18a93c5b8ad3e7ecc03ff
mirzavic4869 <avicenamirzam@gmail.com>
2022-08-11 10:41:39 +0700

    styling history order page

---
+++ pages/toko/history_order.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Link from "next/link";
+
+function History_order() {
+	return (
+		<div className="bg-[#F8F9FD] h-full">
+			<Navbar />
+			<div>
+				<h1 className="text-black font-Roboto font-semibold text-[30px] p-5 text-center md:text-[50px] lg:text-left lg:ml-20">
+					History Order Product
+				</h1>
+				<div className="p-5 gap-4 grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+					<div className="w-auto h-auto bg-white rounded-[20px] shadow-md text-md md:text-lg lg:text-xl text-black font-Poppins font-medium p-5">
+						<p className="pb-3">ID Order</p>
+						<p className="pb-3">10 Juli 2022</p>
+						<p className="pb-3">Terkirim</p>
+						<p className="pb-3">Rp 399.000</p>
+						<div className="flex justify-end">
+							<Link href="/detail_order">
+								<button className="p-3 bg-[#1DB468] text-white rounded-[10px]">
+									Detail
+								</button>
+							</Link>
+						</div>
+					</div>
+					<div className="w-auto h-auto bg-white rounded-[20px] shadow-md text-md md:text-lg lg:text-xl text-black font-Poppins font-medium p-5">
+						<p className="pb-3">ID Order</p>
+						<p className="pb-3">10 Juli 2022</p>
+						<p className="pb-3">Terkirim</p>
+						<p className="pb-3">Rp 399.000</p>
+						<div className="flex justify-end">
+							<Link href="/detail_order">
+								<button className="p-3 bg-[#1DB468] text-white rounded-[10px]">
+									Detail
+								</button>
+							</Link>
+						</div>
+					</div>
+					<div className="w-auto h-auto bg-white rounded-[20px] shadow-md text-md md:text-lg lg:text-xl text-black font-Poppins font-medium p-5">
+						<p className="pb-3">ID Order</p>
+						<p className="pb-3">10 Juli 2022</p>
+						<p className="pb-3">Terkirim</p>
+						<p className="pb-3">Rp 399.000</p>
+						<div className="flex justify-end">
+							<Link href="/detail_order">
+								<button className="p-3 bg-[#1DB468] text-white rounded-[10px]">
+									Detail
+								</button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default History_order;
