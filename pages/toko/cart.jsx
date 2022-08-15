965b897dc9170b23fef6647e36090cd8f18ec39b
mirzavic4869 <avicenamirzam@gmail.com>
2022-08-16 05:25:17 +0700

    change SSR

--- pages/toko/cart.jsx
+++ pages/toko/cart.jsx
@@ -4,121 +4,186 @@ import Link from "next/link";
 import Modal from "../../components/Modal";
 import { MyCart } from "../../components/CartCard";
 import { useRouter } from "next/dist/client/router";
-import { getCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
 
-function Cart() {
-	const [datas, setDatas] = useState([]);
-	const [loading, setLoading] = useState([]);
+export async function getServerSideProps({ req, res }) {
+	const token = getCookie("token", { req, res });
+	if (!token) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/auth/welcome",
+			},
+		};
+	}
+	const requestOptions = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+	const response = await fetch(
+		"https://virtserver.swaggerhub.com/vaniliacahya/capstone/1.0.0/carts",
+		requestOptions
+	);
+	const data = await response.json();
+	if (response.status === 200) {
+		return {
+			props: { code: data.code, data: data.data, message: data.message, token },
+		};
+	} else {
+		deleteCookie("token");
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/auth/welcome",
+			},
+		};
+	}
+}
+
+function Cart({ datas }) {
 	const token = getCookie("token");
 	const router = useRouter();
 	const [total, setTotal] = useState([]);
 
-	useEffect(() => {
-		if (!token) {
-			router.push("/auth/welcome");
-		}
-		fetchData();
-	}, []);
+	// useEffect(() => {
+	// 	if (!token) {
+	// 		router.push("/auth/welcome");
+	// 	}
+	// 	fetchData();
+	// }, []);
 
-	const fetchData = async () => {
-		const requestOptions = {
-			method: "GET",
+	// const fetchData = async () => {
+	// 	const requestOptions = {
+	// 		method: "GET",
+	// 	};
+
+	// 	fetch("https://postme.site/carts", requestOptions)
+	// 		.then((response) => response.json())
+	// 		.then((result) => {
+	// 			const { code, data } = result;
+	// 			if (code === 200) {
+	// 				setDatas(data.items);
+	// 			}
+	// 			if (code === 200) {
+	// 				setTotal(data);
+	// 			}
+	// 		})
+	// 		.catch((error) => alert(error.toString))
+	// 		.finally(() => setLoading(false));
+	// };
+
+	// update
+	const handleSubmit = async (e) => {
+		setLoading(true);
+		e.preventDefault();
+		const body = { qty };
+
+		let requestOptions = {
+			method: "PUT",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
 		};
 
 		fetch(
-			"https://virtserver.swaggerhub.com/vaniliacahya/capstone/1.0.0/carts",
+			`https://virtserver.swaggerhub.com/vaniliacahya/capstone/1.0.0/carts/1`,
 			requestOptions
 		)
 			.then((response) => response.json())
 			.then((result) => {
-				const { code, data } = result;
-				if (code === 200) {
-					setDatas(data.items);
-				}
-				if (code === 200) {
-					setTotal(data);
-				}
+				console.log(result);
+				const { message } = result;
+				location.reload();
+				alert(message);
 			})
-			.catch((error) => alert(error.toString))
+			.catch((error) => {
+				console.log(error);
+				alert(error.toString());
+			})
 			.finally(() => setLoading(false));
 	};
-	if (loading) {
-		return <div>Please wait...</div>;
-	} else {
-		return (
-			<div className="bg-base-100 min-h-screen">
-				<Navbar />
-				<div>
-					<h1 className="font-Roboto font-semibold text-[30px] p-9 text-center md:text-[44px] lg:text-[44px] lg:text-left lg:ml-20 text-black">
-						My Cart
-					</h1>
-				</div>
-				<div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
-					{datas.map((data) => (
-						<MyCart
-							key={data.id}
-							image={data.product_image}
-							name={data.product_name}
-							unit={data.unit}
-							price={data.price}
-							qty={data.qty}
-							subtotal={data.subtotal}
-						/>
-					))}
 
-					{/* <div className="w-auto h-auto bg-white rounded-[20px] shadow-md flex m-2 justify-center">
-					<img
-						className="p-5 h-[200px] w-[140px]"
-						src="https://images.unsplash.com/photo-1610663711502-35f870cfaea2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-						alt="image"
-					/>
-					<div className="flex flex-col  font-Poppins font-medium   ">
-						<div className="flex flex-row">
-							<h2 className="py-2 pr-8 text-black">Beras Wangi</h2>
-						</div>
-						<h3 className="pb-4 text-black">5 Kg</h3>
-						<h3 className="pb-4 text-black">Rp 40.000</h3>
+	// delete user
+	const handleDelete = () => {
+		setLoading(true);
+		var requestOptions = {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+		};
 
-						<div className="flex flex-row">
-							<h3 className="pb-4 text-black">Ammount</h3>{" "}
-							<button className="bg-white border-[#1DB468] border-2 rounded-[5px] w-[33px] h-[33px] ml-3 text-black">
-								-
-							</button>
-							<p className="ml-3 text-black">3</p>
-							<button className="bg-[#1DB468] rounded-[5px] w-[33px] h-[33px] text-white ml-3">
-								+
-							</button>
-						</div>
-						<h3 className="text-black">Total Rp 120.000</h3>
-					</div>
-					<div className="relative">
-						<div className="absolute top-0 -right-3 lg:-right-10">
-							<label
-								id="btn-delete"
-								htmlFor="modal-delete"
-								className="p-2 btn btn-secondary text-white rounded-[10px]"
-							>
-								Delete
-							</label>
-						</div>
-					</div> */}
-				</div>
+		fetch(
+			"https://virtserver.swaggerhub.com/vaniliacahya/capstone/1.0.0/carts/1",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				const { message, code } = result;
+				if (code === 200) {
+					alert(message);
+					deleteCookie("token");
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+			.finally(() => setLoading(false));
+	};
 
-				<div className="w-auto h-auto bg-white rounded-[20px] shadow-md m-2 flex justify-between font-Poppins font-semibold p-3 text-black text-lg">
-					<p>Total Price Rp {total.grand_total}</p>
-					<Link href="/toko/history_order">
-						<button
-							id="to-payment"
-							className="py-2 px-8 btn btn-primary text-white rounded-[10px]"
-						>
-							Next
-						</button>
-					</Link>
-				</div>
-				<Modal id="modal-delete" title="Delete Product" />
+	return (
+		<div className="bg-base-100 min-h-screen">
+			<Navbar />
+			<div>
+				<h1 className="font-Roboto font-semibold text-[30px] p-9 text-center md:text-[44px] lg:text-[44px] lg:text-left lg:ml-20 text-black">
+					My Cart
+				</h1>
 			</div>
-		);
-	}
+			<div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
+				{datas.map((data) => (
+					<MyCart
+						key={data.id}
+						image={data.product_image}
+						name={data.product_name}
+						unit={data.unit}
+						price={data.price}
+						qty={data.qty}
+						subtotal={data.subtotal}
+					/>
+				))}
+				<div className="relative">
+					<div className="absolute top-0 -right-2 md:right-0 lg:-right-5">
+						<label
+							id="btn-delete"
+							// htmlFor="modal-delete"
+							className="p-2 btn btn-secondary text-white rounded-[10px]"
+							onClick={() => handleDelete()}
+						>
+							Delete
+						</label>
+					</div>
+				</div>
+			</div>
+
+			<div className="w-auto h-auto bg-white rounded-[20px] shadow-md m-2 flex justify-between font-Poppins font-semibold p-3 text-black text-lg">
+				<p>Total Price Rp {total.grand_total}</p>
+				<Link href="/toko/history_order">
+					<button
+						id="to-payment"
+						className="py-2 px-8 btn btn-primary text-white rounded-[10px]"
+					>
+						Next
+					</button>
+				</Link>
+			</div>
+			{/* <Modal id="modal-delete" title="Delete Product" /> */}
+		</div>
+	);
 }
 
 export default Cart;
