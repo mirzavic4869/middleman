867666bab294661721c17892839da4aa9304fe0a
mirzavic4869 <avicenamirzam@gmail.com>
2022-08-18 21:43:33 +0700

    added post

--- pages/grosir/inbound.jsx
+++ pages/grosir/inbound.jsx
@@ -5,89 +5,136 @@ import { getCookie } from "cookies-next";
 import { useRouter } from "next/dist/client/router";
 
 function Inbound() {
-  const token = getCookie("token");
-  const router = useRouter();
-  const [datas, setDatas] = useState([]);
-  const [loading, setLoading] = useState(false);
+	const token = getCookie("token");
+	const router = useRouter();
+	const [datas, setDatas] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (!token) {
-      router.push("/auth/welcome");
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		if (!token) {
+			router.push("/auth/welcome");
+		}
+		fetchData();
+	}, []);
 
-  const fetchData = async () => {
-    setLoading(true);
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
+	const fetchData = async () => {
+		setLoading(true);
+		const requestOptions = {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
 
-    fetch("https://postme.site/inoutbounds", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        const { code, data } = result;
-        if (code === 200) {
-          setDatas(data.items.reverse());
-        }
-      })
-      .catch((error) => alert(error.toString))
-      .finally(() => setLoading(false));
-  };
+		fetch("https://postme.site/inoutbounds", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				const { code, data } = result;
+				if (code === 200) {
+					setDatas(data.items.reverse());
+				}
+			})
+			.catch((error) => alert(error.toString))
+			.finally(() => setLoading(false));
+	};
 
-  const deleteData = async (e, idProduct) => {
-    e.preventDefault();
-    const requestOptions = {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
+	const addData = async (e, key, qty, unit) => {
+		e.preventDefault();
+		// const body = {
+		// 	product_id: key,
+		// 	qty: qty,
+		// 	unit: unit,
+		// };
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+			body: datas.items,
+		};
 
-    fetch(`https://postme.site/inoutbounds/${idProduct}`, requestOptions)
-      .then((result) => {
-        alert("Berhasil hapus data");
-      })
-      .catch((error) => alert(error.toString()))
-      .finally(() => {
-        fetchData();
-      });
-  };
+		fetch("https://postme.site/admins/inventory", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				const { message } = result;
+				alert(message);
+			})
+			.catch((error) => alert(error.toString))
+			.finally(() => {
+				fetchData();
+			});
+	};
 
-  return (
-    <div className="bg-base-100 w-full h-screen">
-      <Navbar />
-      <div>
-        <h1 className="font-Roboto font-semibold text-[30px] p-3 text-center md:text-[44px] lg:text-[44px] lg:text-left lg:ml-20 text-black">Inbound Product</h1>
-      </div>
-      <div className="flex justify-end m-3">
-        <button id="to-history-outbound" onClick={() => router.push("/history-product-in")} className="btn btn-primary btn-sm text-white font-Roboto">
-          History Product In
-        </button>
-      </div>
-      {datas ? (
-        loading ? (
-          <div className="text-center">Loading...</div>
-        ) : (
-          <div className="grid gap-2 m-2 grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
-            {datas.map((data) => (
-              <OutBound key={data.product_id} id={data.product_id} name={data.product_name} unit={data.unit} qty={data.qty} fnDeleteData={deleteData} />
-            ))}
-          </div>
-        )
-      ) : (
-        <div className="text-center">Please add your products</div>
-      )}
-      <div className="fixed bottom-0 right-0 m-3">
-        <button id="btn-submit" onClick={(e) => addData(e)} className={`btn btn-primary ${datas ? null : "btn-disabled"} btn text-white font-Roboto`}>
-          Submit
-        </button>
-      </div>
-    </div>
-  );
+	const deleteData = async (e, idProduct) => {
+		e.preventDefault();
+		const requestOptions = {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		fetch(`https://postme.site/inoutbounds/${idProduct}`, requestOptions)
+			.then((result) => {
+				alert("Success delete product");
+			})
+			.catch((error) => alert(error.toString()))
+			.finally(() => {
+				fetchData();
+			});
+	};
+
+	return (
+		<div className="bg-base-100 w-full h-screen">
+			<Navbar />
+			<div>
+				<h1 className="font-Roboto font-semibold text-[30px] p-3 text-center md:text-[44px] lg:text-[44px] lg:text-left lg:ml-20 text-black">
+					Inbound Product
+				</h1>
+			</div>
+			<div className="flex justify-end m-3">
+				<button
+					id="to-history-outbound"
+					onClick={() => router.push("/history-product-in")}
+					className="btn btn-primary btn-sm text-white font-Roboto"
+				>
+					History Product In
+				</button>
+			</div>
+			{datas ? (
+				loading ? (
+					<div className="text-center">Loading...</div>
+				) : (
+					<div className="grid gap-2 m-2 grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
+						{datas.map((data) => (
+							<OutBound
+								key={data.product_id}
+								id={data.product_id}
+								name={data.product_name}
+								unit={data.unit}
+								qty={data.qty}
+								fnDeleteData={deleteData}
+							/>
+						))}
+					</div>
+				)
+			) : (
+				<div className="text-center">Please add your products</div>
+			)}
+			<div className="fixed bottom-0 right-0 m-3">
+				<button
+					id="btn-submit"
+					onClick={(e) => addData(e)}
+					className={`btn btn-primary ${
+						datas ? null : "btn-disabled"
+					} btn text-white font-Roboto`}
+				>
+					Submit
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Inbound;
