f0a1c18db8ac95e424af4bd57026c15f0ab50bb7
mirzavic4869 <avicenamirzam@gmail.com>
2022-08-18 11:31:18 +0700

    fixing my cart

--- components/CartCard.jsx
+++ components/CartCard.jsx
@@ -1,85 +1,121 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState, useEffect } from "react";
+import { formatCurrency } from "./CardProduct";
 
 function MyCart(props) {
-  return (
-    <div className="card card-side bg-white shadow-md">
-      <figure>
-        <img src={props.image} alt="image" />
-      </figure>
-      <div className="card-body font-Poppins">
-        <h2 className="card-title">{props.name}</h2>
-        <p>{props.unit}</p>
-        <p>Rp {props.price}</p>
-        <p>Ammount</p>
-        <p>{props.qty}</p>
-        <p>{props.subtotal}</p>
-        <div className="flex">
-          <button className="bg-white btn btn-outline btn-primary rounded-[5px] w-[50px] text-black" onSubmit={(e) => props.handleSubmit(e, props.product_id)} onClick={() => props.handleDecrement()}>
-            -
-          </button>
-          <p>{props.counter}</p>
-          <button className="btn btn-primary rounded-[5px] w-[50px] text-white ml-8" onSubmit={(e) => props.handleSubmit(e, props.product_id)} onClick={() => props.handleIncrement()}>
-            +
-          </button>
-        </div>
+	const [counter, setCounter] = useState(1);
+	const handleIncrement = () => {
+		setCounter(counter + 1);
+	};
 
-        <div className="card-actions justify-end">
-          <button className="btn btn-secondary text-white" onClick={(e) => props.handleDelete(e)}>
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	const handleDecrement = () => {
+		setCounter(counter - 1);
+	};
+
+	return (
+		<div className="card card-side bg-white shadow-md">
+			<figure>
+				<img src={props.image} alt="image" />
+			</figure>
+			<div className="card-body font-Poppins">
+				<h1 className="card-title">{props.name}</h1>
+				<p>{props.unit}</p>
+				<p>{formatCurrency(props.price)}</p>
+
+				<p>Ammount</p>
+				<div className="flex">
+					<button
+						className="bg-white btn btn-outline btn-primary btn-sm text-black text-lg"
+						onClick={() => handleDecrement()}
+					>
+						-
+					</button>
+					<p className="text-center w-20">{counter} </p>
+					<button
+						className="btn btn-primary btn-sm text-white text-lg"
+						onClick={() => handleIncrement()}
+					>
+						+
+					</button>
+				</div>
+				<p>Sub Total : {formatCurrency(props.price * counter)}</p>
+
+				<div className="card-actions justify-end">
+					<button
+						className="btn btn-secondary text-white"
+						onClick={(e) => props.handleDelete(e, props.id)}
+					>
+						Delete
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 function OutBound(props) {
-  const [value, setValue] = useState(1);
-  function Increment() {
-    setValue(value + 1);
-  }
-  function Decrement() {
-    setValue(value - 1);
-  }
+	const [value, setValue] = useState(1);
+	function Increment() {
+		setValue(value + 1);
+	}
+	function Decrement() {
+		setValue(value - 1);
+	}
 
-  useEffect(() => {
-    if (value < 1) {
-      setValue(0);
-    }
-    if (value > props.qty) {
-      setValue(props.qty);
-    }
-  }, [value]);
+	useEffect(() => {
+		if (value < 1) {
+			setValue(0);
+		}
+		if (value > props.qty) {
+			setValue(props.qty);
+		}
+	}, [value]);
 
-  return (
-    <div className="card w-auto bg-white shadow-md">
-      <div className="card-body font-Poppins">
-        <div className="card-actions justify-end">
-          <button id="btn-delete" onClick={(e) => props.fnDeleteData(e, props.id)} title="Delete" className="btn btn-secondary text-white font-Roboto rounded-full">
-            Delete
-          </button>
-        </div>
-        <h2 className="card-title">{props.name}</h2>
-        <p>Unit: {props.unit}</p>
-        <p>Stock: {props.qty}</p>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Enter amount</span>
-          </label>
-          <div className="input-group">
-            <button id="decrement" onClick={() => Decrement()} className="btn btn-outline btn-sm btn-primary text-lg">
-              -
-            </button>
-            <input type="number" value={value} className="border input-sm input-primary w-20" />
-            <button id="increment" onClick={() => Increment()} className="btn btn-primary btn-sm text-white text-lg">
-              +
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card w-auto bg-white shadow-md">
+			<div className="card-body font-Poppins">
+				<div className="card-actions justify-end">
+					<button
+						id="btn-delete"
+						onClick={(e) => props.fnDeleteData(e, props.id)}
+						title="Delete"
+						className="btn btn-secondary text-white font-Roboto rounded-[10px]"
+					>
+						Delete
+					</button>
+				</div>
+				<h2 className="card-title">{props.name}</h2>
+				<p>Unit: {props.unit}</p>
+				<p>Stock: {props.qty}</p>
+				<div className="form-control">
+					<label className="label">
+						<span className="label-text">Enter amount</span>
+					</label>
+					<div className="input-group">
+						<button
+							id="decrement"
+							onClick={() => Decrement()}
+							className="btn btn-outline btn-sm btn-primary text-lg"
+						>
+							-
+						</button>
+						<input
+							type="number"
+							value={value}
+							className="border input-sm input-primary w-20"
+						/>
+						<button
+							id="increment"
+							onClick={() => Increment()}
+							className="btn btn-primary btn-sm text-white text-lg"
+						>
+							+
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export { MyCart, OutBound };
--- pages/toko/cart.jsx
+++ pages/toko/cart.jsx
@@ -1,54 +1,87 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Link from "next/link";
-import Modal from "../../components/Modal";
 import { MyCart } from "../../components/CartCard";
 import { useRouter } from "next/dist/client/router";
-import { deleteCookie, getCookie } from "cookies-next";
+import { getCookie } from "cookies-next";
+import { formatCurrency } from "../../components/CardProduct";
 
-export async function getServerSideProps({ req, res }) {
-	const token = getCookie("token", { req, res });
-	if (!token) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/auth/welcome",
-			},
-		};
-	}
-	const requestOptions = {
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
-	const response = await fetch("https://postme.site/carts", requestOptions);
-	const data = await response.json();
+// export async function getServerSideProps({ req, res }) {
+// 	const token = getCookie("token", { req, res });
+// 	if (!token) {
+// 		return {
+// 			redirect: {
+// 				permanent: false,
+// 				destination: "/auth/welcome",
+// 			},
+// 		};
+// 	}
 
-	return {
-		props: {
-			code: data.code,
-			data: data.data.items,
-			message: data.message,
-			token,
-		},
-	};
-}
+// 	const requestOptions = {
+// 		method: "GET",
+// 		headers: {
+// 			Authorization: `Bearer ${token}`,
+// 		},
+// 	};
+// 	const response = await fetch("https://postme.site/carts", requestOptions);
+// 	const data = await response.json();
 
-function Cart({ data }) {
+// 	return {
+// 		props: {
+// 			code: data.code,
+// 			data: data.data.items,
+// 			message: data.message,
+// 			token,
+// 		},
+// 	};
+// }
+
+function Cart() {
 	const token = getCookie("token");
 	const router = useRouter();
 	const [total, setTotal] = useState([]);
 	const [loading, setLoading] = useState();
-	const [counter, setCounter] = useState(1);
+	const [datas, setDatas] = useState([]);
+
+	useEffect(() => {
+		if (!token) {
+			router.push("/auth/welcome");
+		}
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		setLoading(true);
+		const requestOptions = {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
+
+		fetch("https://postme.site/carts", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				const { code, data } = result;
+				if (code === 200) {
+					setDatas(data.items.reverse());
+				}
+				if (code === 200) {
+					setTotal(data);
+				}
+			})
+			.catch((error) => alert(error.toString))
+			.finally(() => setLoading(false));
+	};
 
 	// update
-	const handleSubmit = async (e, qty) => {
+	const handleSubmit = async (e, qty, product_id) => {
 		setLoading(true);
 		e.preventDefault();
 		const body = { qty };
 
-		let requestOptions = {
+		const requestOptions = {
 			method: "PUT",
 			headers: {
 				Authorization: `Bearer ${token}`,
@@ -57,7 +90,7 @@ function Cart({ data }) {
 			body: JSON.stringify(body),
 		};
 
-		fetch(`https://postme.site/carts/1`, requestOptions)
+		fetch(`https://postme.site/carts/${product_id}`, requestOptions)
 			.then((response) => response.json())
 			.then((result) => {
 				const { message } = result;
@@ -71,40 +104,25 @@ function Cart({ data }) {
 			.finally(() => setLoading(false));
 	};
 
-	const handleIncrement = () => {
-		setCounter(counter + 1);
-	};
-
-	const handleDecrement = () => {
-		setCounter(counter - 1);
-	};
-
 	// delete user
-	const handleDelete = (e, product_id) => {
-		setLoading(true);
-		var requestOptions = {
+	const handleDelete = async (e, product_id) => {
+		e.preventDefault();
+		const requestOptions = {
 			method: "DELETE",
 			headers: {
 				Authorization: `Bearer ${token}`,
-				"Content-Type": "application/json",
 			},
 		};
 
-		fetch("https://postme.site/carts/1", requestOptions)
-			.then((response) => response.json())
+		fetch(`https://postme.site/carts/${product_id}`, requestOptions)
 			.then((result) => {
-				const { message, code } = result;
-				if (code === 200) {
-					alert(message);
-					deleteCookie("token");
-				}
+				alert("success delete product");
 			})
-			.catch((error) => {
-				console.log(error);
-			})
-			.finally(() => setLoading(false));
+			.catch((error) => alert(error.toString))
+			.finally(() => {
+				fetchData();
+			});
 	};
-
 	return (
 		<div className="bg-base-100 min-h-screen">
 			<Navbar />
@@ -114,25 +132,23 @@ function Cart({ data }) {
 				</h1>
 			</div>
 			<div className="mx-5 gap-5 grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center">
-				{data.map((data) => (
+				{datas.map((data) => (
 					<MyCart
-						key={data.product.id}
-						image={data.product.product_image}
-						name={data.product.product_name}
-						unit={data.product.unit}
-						price={data.product.price}
-						qty={data.qty}
+						key={data.product_id}
+						id={data.product_id}
+						image={data.product_image}
+						name={data.product_name}
+						unit={data.unit}
+						price={data.price}
 						subtotal={data.subtotal}
 						handleSubmit={handleSubmit}
 						handleDelete={handleDelete}
-						handleIncrement={handleIncrement}
-						handleDecrement={handleDecrement}
 					/>
 				))}
 			</div>
 
 			<div className="w-auto h-auto bg-white rounded-[20px] shadow-md m-5 flex justify-between font-Poppins font-semibold p-3 text-black text-lg">
-				<p>Total Price Rp {total.grand_total}</p>
+				<p>Total Price {formatCurrency(total.grand_total)}</p>
 				<Link href="/toko/history_order">
 					<button
 						id="to-payment"
