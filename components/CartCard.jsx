854cffc93c4146e3e2f0a7e34c6a9e641c81b8aa
Afrizal <4friizal@gmail.com>
2022-08-20 00:29:46 +0700

    fixing import function

--- components/CartCard.jsx
+++ components/CartCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { formatCurrency } from "./CardProduct";
+import { formatCurrency } from "../pages/inventory";
 
 function MyCart(props) {
 	return (
