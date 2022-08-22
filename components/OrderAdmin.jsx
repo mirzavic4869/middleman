91c82d8c0dcb45a60b7a42d27673cc343c91ac2f
Shodiq Farhan Jamil <shodiqfarhan24@gmail.com>
2022-08-22 13:19:57 +0700

    fix get incoming order

--- components/OrderAdmin.jsx
+++ components/OrderAdmin.jsx
@@ -21,25 +21,21 @@ function HistoryAdminOrder(props) {
 
 function IncomingOrder(props) {
   return (
-    <div className="mx-auto">
-      <table className="w-full table-fixed font-Poppins">
-        <tbody>
-          <tr className="bg-white text-xs md:text-lg lg:text-lg text-black border-b-2 border-neutral-400 ">
-            <td className="py-2 pl-5">{props.id}</td>
-            <td className="py-2">{formatDate(props.date)}</td>
-            <td className="py-2">{props.status}</td>
-            <td className="py-2">{formatCurrency(props.total)}</td>
-            <td className="py-2">
-              <Link href={`/grosir/${props.id}`}>
-                <button id="to-detail-order" className="btn btn-primary btn-sm text-white rounded-[10px]">
-                  Detail
-                </button>
-              </Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <tbody>
+      <tr className="bg-white text-xs md:text-lg lg:text-lg text-black border-b-2 border-neutral-400 text-center">
+        <td className="py-2">{props.id}</td>
+        <td className="py-2">{formatDate(props.date)}</td>
+        <td className="py-2">{props.status}</td>
+        <td className="py-2">{formatCurrency(props.total)}</td>
+        <td className="py-2">
+          <Link href={`/grosir/${props.id}`}>
+            <button id="to-detail-order" className="btn btn-primary btn-sm text-white rounded-[10px]">
+              Detail
+            </button>
+          </Link>
+        </td>
+      </tr>
+    </tbody>
   );
 }
 
--- pages/grosir/incoming_product.jsx
+++ pages/grosir/incoming_product.jsx
@@ -42,20 +42,20 @@ function Incoming_product({ data }) {
         <h1 className="text-black font-Roboto font-semibold text-[30px] p-9 text-center md:text-[44px] lg:text-[44px] lg:text-left lg:ml-20">Incoming Order Product</h1>
         {data ? (
           <div className="mx-auto md:mx-16">
-            <table className="w-full table-fixed">
+            <table className="w-full table-fluid">
               <thead>
-                <tr className="bg-[#EEEEEE] text-xs md:text-lg lg:text-lg font-bold text-black">
-                  <td className="py-2 pl-5">ID</td>
+                <tr className="bg-[#EEEEEE] text-xs md:text-lg lg:text-lg font-bold text-black text-center">
+                  <td className="py-2">ID</td>
                   <td className="py-2">DATE</td>
                   <td className="py-2">STATUS</td>
                   <td className="py-2">TOTAL PRICE</td>
                   <td className="py-2"></td>
                 </tr>
               </thead>
+              {data.map((value) => (
+                <IncomingOrder key={value.order_id} id={value.order_id} date={value.date} status={value.status} total={value.grand_total} />
+              ))}
             </table>
-            {data.map((value) => (
-              <IncomingOrder key={value.id} id={value.order_id} date={formatDate(value.date)} status={value.status} total={value.grand_total} />
-            ))}
           </div>
         ) : (
           <div className="flex  justify-center items-center text-lg md:text-3xl font-Roboto font-bold text-slate-700/20">Empty data</div>
